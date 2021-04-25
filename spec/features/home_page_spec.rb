4fa588168ec481cbca9733f26b3d7c1daf7ec2d6
Colter187 <Colter_187@aol.com>
2021-04-25 19:12:59 +0000

    wowwie

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,5 +4,7 @@ RSpec.feature "Visiting the homepage", type: :feature do
   scenario "The visitor should see projects" do
     visit root_path
     expect(page).to have_text("Projects")
+    expect(page).to have_text("Title")
+    expect(page).to have_text("Description")
   end
 end
